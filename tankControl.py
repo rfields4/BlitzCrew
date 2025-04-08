1da7292e8e1ed3732bda63962082147fe342f277
Robert <rfields4@ycp.edu>
2025-04-08 18:25:24 +0100

    Added PWM code to control Tank

--- tankControl.py
+++ tankControl.py
@@ -1,3 +1,4 @@
+
 import RPi.GPIO as GPIO
 from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import AngularServo
@@ -8,20 +9,29 @@ import RPi.GPIO as GPIO
 
 def cameraAngleControl():
 
-    leftTreadPin = 21 #PIN 40
-    rightTreadPin = 26 #PIN 37
-
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(leftTreadPin, GPIO.OUT)
-    GPIO.setup(rightTreadPin, GPIO.OUT)
+
+    leftTreadPin1  = 27 #PIN 13 ForwardPin
+    GPIO.setup(27, GPIO.OUT)
+    leftTreadPin2  = 22 #PIN 15 BackPin
+    GPIO.setup(22, GPIO.OUT)
+    rightTreadPin1 = 23 #PIN 16 ForwardPin
+    GPIO.setup(23, GPIO.OUT)
+    rightTreadPin2 = 24 #Pin 18 BackPin
+    GPIO.setup(24, GPIO.OUT)
+
+    leftpwmPin = 5 #PIN 29
+    GPIO.setup(leftpwmPin, GPIO.OUT)
+    rightpwmPin= 6 #PIN 31
+    GPIO.setup(rightpwmPin, GPIO.OUT)
     
     commsPort = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     ipAddr = "192.168.69.69"
     commsPort.bind((ipAddr, 2222))    
 
-    leftpwmPin = GPIO.PWM(leftTreadPin, 100)
-    rightpwmPin = GPIO.PWM(rightTreadPin, 100)
+    leftpwmPin = GPIO.PWM(5, 100)
+    rightpwmPin = GPIO.PWM(6, 100)
     
     leftpwmPin.start(0)
     rightpwmPin.start(0)
@@ -49,16 +59,45 @@ def cameraAngleControl():
                     sleep(0.000001)
                     print(servoVertical.angle)
                 if data == b"LRight":
-                    rightpwmPin.ChangeDutyCycle(100)
+                    GPIO.output(27, GPIO.HIGH)
+                    GPIO.output(22, GPIO.LOW)
+                    GPIO.output(23, GPIO.LOW)
+                    GPIO.output(24, GPIO.HIGH)
+
+                    rightpwmPin.ChangeDutyCycle(30)
+                    leftpwmPin.ChangeDutyCycle(60)
                     sleep(0.01)
                 if data == b"LLeft":
-                    leftpwmPin.ChangeDutyCycle(100)
+                    GPIO.output(27, GPIO.LOW)
+                    GPIO.output(22, GPIO.HIGH)
+                    GPIO.output(23, GPIO.HIGH)
+                    GPIO.output(24, GPIO.LOW)
+
+                    rightpwmPin.ChangeDutyCycle(60)
+                    leftpwmPin.ChangeDutyCycle(30)
                     sleep(.01)
                 if data == b"LUp":
-                    leftTreadPin.ChangeDutyCycle(100)
-                    leftTreadPin.ChangeDutyCycle(100)
-                    sleep(.01)
-                
+                    GPIO.output(27, GPIO.HIGH)
+                    GPIO.output(22, GPIO.LOW)
+                    GPIO.output(23, GPIO.HIGH)
+                    GPIO.output(24, GPIO.LOW)
+
+                    rightpwmPin.ChangeDutyCycle(80)
+                    leftpwmPin.ChangeDutyCycle(80)
+                    sleep(.1)
+                    rightpwmPin.ChangeDutyCycle(0)
+                    leftpwmPin.ChangeDutyCycle(0)
+                if data == b'LDown':
+                    GPIO.output(27, GPIO.LOW)
+                    GPIO.output(22, GPIO.HIGH)
+                    GPIO.output(23, GPIO.LOW)
+                    GPIO.output(24, GPIO.HIGH)
+
+                    rightpwmPin.ChangeDutyCycle(80)
+                    leftpwmPin.ChangeDutyCycle(80)
+                    sleep(.1)
+                    rightpwmPin.ChangeDutyCycle(0)
+                    leftpwmPin.ChangeDutyCycle(0)
                 if data == b"RLeft" and servoHorizontal.angle + 7 < 300:
                     servoHorizontal.angle = servoHorizontal.angle + 7
                     sleep(0.000001)
